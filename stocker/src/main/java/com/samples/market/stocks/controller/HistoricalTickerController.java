105cf012f310b8f3c01e6fbb1925035fdd3d6077
chiusday <chiusday@hotmail.sg>
2019-03-23 20:09:16 +0800

    Cleared warnings and comments

--- stocker/src/main/java/com/samples/market/stocks/controller/HistoricalTickerController.java
+++ stocker/src/main/java/com/samples/market/stocks/controller/HistoricalTickerController.java
@@ -2,8 +2,6 @@ package com.samples.market.stocks.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +14,6 @@ import com.samples.market.stocks.visitor.model.HistoricalTickerListVisitorModel;
 public class HistoricalTickerController {
 	@Autowired
 	private HistoricalTickerService historicalTickerService;
-	
-//	@GetMapping("/stock/list/{id}")
-//	public ResponseEntity<Object> getJsonList(@PathVariable String id){
-//		HistoricalTickerListVisitorModel historicalTickerVisitorModel = 
-//				historicalTickerService.getHistoricalTickers(id); 
-//		
-//		return historicalTickerVisitorModel.getResponseEntity();
-//	}
 	
 	@PostMapping("/stock/list")
 	public ResponseEntity<Object> getJson(@RequestBody GetBySymbolRequest request){
